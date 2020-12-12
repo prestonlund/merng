d3c8a8be3f26333f1a86a95802e53d80e5da05d0
Preston Lund <prestonlund@gmail.com>
2020-12-12 13:54:48 -0700

    Authentication middleware, create and delete posts

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const { MONGODB } = require('./config.js');
 // Setup Apollo server
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }) => ({ req })
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
